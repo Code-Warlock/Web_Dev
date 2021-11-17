b37f4b093c8e9d6d244e7303229b40db7237cc4f
Code-Warlock <dessymichaeltgp123@gmail.com>
2021-11-17 14:02:45 +0100

    Gender & Gendertypes

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// Variables and Datatypes.
+// var number = 4;
+// var name = "Desmond";
+// var isLate = false;
+// console.log(number);
+// console.log(name);
+// console.log(isLate);
+var firstName = prompt("Enter your name ");
+let lastName = "Michael"; // ES6 or ES2015
+const noOfEars = 2;
+const noOfEars = 3;
+// var age = parseInt(prompt("Enter your age "));
+// var age = parseFloat(prompt("Enter your age "));
+var age = Number(prompt("Enter your age "));
+// parseInt();
+// parseFloat();
+var isLate = false;
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(firstName);
+console.log(age);
