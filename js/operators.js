def70ed30e4295c251ecac9fee07e5f2fcf56675
Code-Warlock <dessymichaeltgp123@gmail.com>
2021-11-19 13:30:43 +0100

    Operators and String Concatenation

---
+++ js/operators.js
@@ -0,0 +1,22 @@
+/*JS Operators*/
+//Arithmetic , Assignment , Comparison , Logical
+// let x = 9 , y = 90 , z = 100;
+// let x , y ,z;
+// let x = 9;
+// x += 90;
+// console.log(x);
+// console.log(8!=8);
+
+
+
+
+// let age = parseInt(prompt("Enter your age"));
+// let gender = prompt("Male or Female ? ")
+// let canEnterClub = !(age >= 18 && gender == "Male");
+// // let canEnterClub = age >= 18 || gender == "Male";
+// console.log(canEnterClub);
+
+// String Methods
+let firstName = prompt("Enter your first name");
+let lastName = prompt("Enter your last name");
+console.log(firstName + " " + lastName);
