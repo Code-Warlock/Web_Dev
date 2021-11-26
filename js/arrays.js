772cbc311fe124feea654b26a696b652000a67dc
Code Warlock <dessymichaeltgp123@gmail.com>
2021-11-26 13:49:26 +0100

    Array Methods ,While loops and do-While Loops

--- js/arrays.js
+++ js/arrays.js
@@ -8,7 +8,39 @@
 // let myNameList = myName.split(" ");
 //
 //
-// 
+//
 //
 // let refinedString = myNameList.join(" ");
 // console.log(refinedString);
+
+
+// Array Methods
+let students = ["Desmond","Michael","Code","Warlock"];
+let femaleStudents = ["Desmondina","Michaellia","Codress","Witch"];
+let ages = [12,34,56,78,9]
+let everybody  = femaleStudents.concat(students,ages,"HAAAA");
+// Shift , unshift , pop and push
+// everybody.shift();
+// everybody.pop();
+
+
+// unshift and push
+
+
+// everybody.push("Cxtus");
+// delete everybody[6];
+// students.splice(students.length,0,"Michael");
+// console.log(students.at(students.length-1));
+// console.log(students.reverse());
+// students[0] = "Michael"
+//
+// console.log(typeof students);
+
+// console.log(students.slice(0,4));
+
+// let myName = "Desmond";
+// myName[0] = "F";
+// console.log(myName[myName.length - 1])
+
+// students[0] = "MyName";
+// console.log(students);
--- js/loops.js
+++ js/loops.js
@@ -1,18 +1,47 @@
 //
 const passwordGen = "123qwerty";
-let password = prompt("Enter your password");
-// password == passwordGen ? console.log("Valid!") : console.log("Wrong Password");
+// let password = prompt("Enter your password");
+// // password == passwordGen ? console.log("Valid!") : console.log("Wrong Password");
+// //
+// // let x = 9;
+// // console.log(x++);//9
+// // console.log(x); //10
 //
-// let x = 9;
-// console.log(x++);//9
-// console.log(x); //10
+// for(let i = 1;i < 3;){
+//   // password == passwordGen ? console.log("Valid!") : console.log("Wrong Password");
+//   if(password == passwordGen){
+//     console.log("Valid!");
+//     break
+//   }
+//     password = prompt("Invalid Password\nEnter your password again");
+//   // break & continue
+// }
 
-for(let i = 1;i < 3;){
-  // password == passwordGen ? console.log("Valid!") : console.log("Wrong Password");
-  if(password == passwordGen){
-    console.log("Valid!");
-    break
-  }
-    password = prompt("Invalid Password\nEnter your password again");
-  // break & continue
-}
+
+
+// While Loops
+count = 10
+// while(password != passwordGen){
+//   password = prompt("Enter your password");
+// }
+// alert("Correct password");
+
+
+// while(count<10){
+//   if(count == 5){
+//     break;
+//     count++;
+//   }
+//   else{
+//     console.log(count);
+//     count++;
+//   }
+// }
+// while(count<10){
+//   console.log(count);
+// }
+
+do{
+  console.log(count);
+  count++;
+}while(count < 10);
