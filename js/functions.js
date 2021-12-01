14db502314dc3e7faa53d7191ec4d9ee22b98464
Code-Warlock <dessymichaeltgp123@gmail.com>
2021-12-01 13:52:13 +0100

    Types of Traps and LGBTQ

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,55 @@
 // Creating a basic function in JavaScript
 
-function sayWelcome(){
-  let x = 90;
-  alert("Welcome to JS");
+// function sayWelcome(name , status){
+//   let x = 90;
+//   if(status.toLowerCase().includes("begin")){
+//     return "Welcome to JS" + " , " + name;
+//   }
+//   else{
+//     return "So where , " + name + " , did you stop?";
+//   }
+// }
+// console.log(name , status)
+// console.log(sayWelcome("Kunle","Expert"));
+// console.log(x);
+//
+//
+// let x = 6;
+// function higherOrder(){
+//
+//   function increaseX(){
+//     return x ** 2;
+//   }
+//   return increaseX();
+// }
+// console.log(
+// (
+//     function(){
+//   return "I just created an anonymous function";
+// }
+// )()
+//
+// );
+
+function palindromeChecker(word){
+  let wordAsList = word.split("");
+  wordAsList.reverse();
+  let refinedWord = wordAsList.join("");
+  return refinedWord == word
+}
+while(true){
+  let userWord = prompt("Enter a word");
+  palindromeChecker(userWord.toLowerCase()) ? alert(userWord + " is a palindrome") : alert(userWord + " is not a palindrome");
+  let continueCheck = prompt("Do you have another word").toLowerCase();
+  if(continueCheck.includes("n") || continueCheck == ""){
+    break;
+  }
+  // else{
+  //   continue;
+  // }
 }
 
 
-sayWelcome();
-console.log(x);
+// console.log(x);
+
+// console.log(speakAnon());
