16bee940131ca6da109bc16c5b0ad3356567cf48
Code-Warlock <dessymichaeltgp123@gmail.com>
2021-12-06 13:47:51 +0100

    DOM and Events

---
+++ js/dom.js
@@ -0,0 +1,31 @@
+// The DOM in JS
+
+// const NAVBAR = document.getElementById("main-nav");
+// const NAVITEMS = document.getElementsByClassName("navitem");
+// let sections = document.getElementsByTagName("section");
+// console.log(NAVBAR);
+// console.log(NAVITEMS);
+// console.log(sections);
+
+
+// DOM OBJECT ATTRIBUTES.
+// console.log(NAVBAR.innerText);
+// for(let i = 0;i<sections.length;i++){
+//   sections[i].innerHTML = "This is the " + i+1 + "st section <br>"
+// }
+// NAVBAR.innerHTML = "I have changed the text in the navbar";
+// NAVBAR.style.backgroundColor = "red";
+
+
+
+//
+// NAVBAR.style.backgroundColor = "maroon";
+// NAVBAR.style.backgroundColor = "maroon";
+
+
+
+
+
+const NAVBAR = document.querySelector('#main-nav');
+const NAVITEMS = document.querySelector('.navitem');
+// console.log(NAVITEMS);
